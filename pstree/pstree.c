02e5f3e8b230be4ac050b427772653e53eb803b1
tracer-nju <tracer@nju.edu.cn>
2021-11-17 15:53:10 +0000

    Static hostname: vultr.guest          Icon name: computer-vm            Chassis: vm         Machine ID: 86412ab75e784c81bf0f3f47a8757b20            Boot ID: bb93f4a4eea8410a9632e9bae992c655     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-88-generic       Architecture: x86-64  15:53:10 up 378 days,  7:56,  0 users,  load average: 0.11, 0.08, 0.05

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -3,17 +3,36 @@
 
 #define FINISHED -2
 #define ERROR -1
-#define TRUE 1
-#define FALSE 0
+#define true 1
+#define false 0
+typedef int bool;
 
 enum Option {
   SHOW_PIDS,
   NUMERIC_SORT,
   VERSION,
+
+  // if the string cannot be parsed
+  _UNKNOWN,
 };
 
+bool str_equal(const char* s1, const char* s2) {
+  int i = 0;
+  while (s1[i] && s2[i]) {
+    if (s1[i] != s2[i]) return false;
+    else i++;
+  }
+  // one has not finished.
+  if (s1[i] || s2[i]) return false;
+  return true;
+}
 
-
+enum Option parse_opt_type(char* arg) {
+  if (str_equal(arg, "-p") || str_equal(arg, "--show-pids")) return SHOW_PIDS;
+  if (str_equal(arg, "-n") || str_equal(arg, "--numeric-sort")) return NUMERIC_SORT;
+  if (str_equal(arg, "-V") || str_equal(arg, "--version")) return VERSION;
+  return _UNKNOWN;
+}
 /**
  * @brief parse the opts.
  * 
@@ -26,9 +45,28 @@ int next_opt(int argc, char *argv[], const char* buf) {
   static int cur_arg = 0;
   if (cur_arg < argc) {
     assert(argv[cur_arg]);
-    printf("argv[%d] = %s\n", cur_arg, argv[cur_arg]);
+    enum Option opt = parse_opt_type(argv[cur_arg]);
+    switch (opt)
+    {
+    case SHOW_PIDS:
+      printf("-p OR --show-pids detected\n");
+      break;
+    case NUMERIC_SORT:
+      printf("-n OR --numeric-sort detected\n");
+      break;
+    case VERSION:
+      printf("-V OR --version detected\n");
+      break;
+    case _UNKNOWN:
+      printf("Unknown param detected\n");
+      break;
+    default:
+      printf("You should have not see me...\n");
+      break;
+    }
+    // printf("argv[%d] = %s\n", cur_arg, argv[cur_arg]);
     cur_arg++;
-    return TRUE;
+    return true;
   }
   return FINISHED;
 }
