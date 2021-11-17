ead1c2da8fc58532d345fafce2a438b5e99b5f5f
tracer-nju <tracer@nju.edu.cn>
2021-11-17 15:34:05 +0000

    Static hostname: vultr.guest          Icon name: computer-vm            Chassis: vm         Machine ID: 86412ab75e784c81bf0f3f47a8757b20            Boot ID: bb93f4a4eea8410a9632e9bae992c655     Virtualization: kvm   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 4.15.0-88-generic       Architecture: x86-64  15:34:05 up 378 days,  7:37,  0 users,  load average: 0.00, 0.02, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define FINISHED -2
+#define ERROR -1
+#define true 1
+#define false 0
+
+enum Option {
+  SHOW_PIDS,
+  NUMERIC_SORT,
+  VERSION,
+};
+
+
+/**
+ * @brief parse the opts.
+ * 
+ * @param argc argc in function main's params
+ * @param argv argv in function main's params
+ * @param buf the option value buffer, nothing changed if not exists.
+ * @return the (type: enum Option) of the next option, -1 error and -2 finished.
+ */
+int next_opt(int argc, char *argv[], const char* buf) {
+  static int cur_arg = 0;
+  if (cur_arg < argc) {
+    assert(argv[cur_arg]);
+    printf("argv[%d] = %s\n", cur_arg, argv[cur_arg]);
+    cur_arg++;
+    return TRUE;
+  }
+  return FINISHED;
+}
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+  char buf[200];
+  int type = next_opt(argc, argv, buf);
+  while (type != FINISHED) {
+    type = next_opt(argc, argv, buf);
   }
   assert(!argv[argc]);
   return 0;
